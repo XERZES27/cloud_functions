f0ad025c587882072e0cc212771c6f5d2dfa726b
XERZES <sibtesh@gmail.com>
2020-04-18 20:59:37 +0300

    Saturday Commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,13 +11,14 @@ const firestore = app.firestore();
 //  response.send("Hello from Firebase!");
 // });
 
+
 export const createDocumentForVerifiedUser = functions.https.onRequest(async (request, response) => {
     const userName = request.query.name;
     const userUid = request.query.uid;
     const userEmail = request.query.email;
     const isVerified = (await admin.auth().getUser(userUid)).emailVerified;
     if (isVerified) {
-        
+
         return await firestore.doc(`users/${userUid}`).set(
             {
                 UserName: userName,
@@ -64,9 +65,9 @@ export const createDocumentForVerifiedVendor = functions.https.onRequest(async (
 })
 
 export const createProduct = functions.https.onRequest(async (request, response) => {
-    const Category = request.query.category;
-    const subCategory = request.query.subCategory;
-    const subSubCategory = request.query.subSubCategory;
+    const Catagory = request.query.catagory;
+    const subCatagory = request.query.subCatagory;
+    const subSubCatagory = request.query.subSubCatagory;
     const productName = request.query.productName;
     const vendorUid = request.query.uid;
     const productDescription = request.query.productDescription;
@@ -75,60 +76,169 @@ export const createProduct = functions.https.onRequest(async (request, response)
     typesArray = JSON.parse(typesArray);
     const vendorExists = await (await firestore.doc(`vendors/${vendorUid}`).get()).exists;
     const productExists = await (await firestore.doc(`products/${vendorUid + productName}`).get()).exists;
-
-    if (vendorExists && !productExists) {
-        return await firestore.collection("products").doc(`${vendorUid + productName}`).set(
-            {
-                productName: productName,
-                route: { Category: Category, subCategory: subCategory, subSubCategory: subSubCategory },
-                productDescription: productDescription,
-                hypeDescription: hypeDescription,
-                StorageId: []
-            }, { merge: true }
-        ).then(() => {
-            let ref = firestore.collection("products").doc(`${vendorUid + productName}`);
-            const writePromises: Promise<any>[] = [];
-            typesArray.forEach((element: Map<any, any>) => {
-                writePromises.push(ref.collection("subProducts").add({ subProduct: element }));
-
-            }
-
-            );
-
-            return Promise.all(writePromises);
-
-        }
-
-        ).catch((err) => response.send(`${err}`)).then(async () => {
-
-
-
-            const reference = await firestore.doc(`vendors/${vendorUid}`).set(
-                {
-                    productName: [`${vendorUid + productName}`]
-
-                }, { merge: true }
-
-            );
-
-            return Promise.resolve(reference)
-        }).then(() => response.send("->>>")).catch((err) => {
-            console.log(err);
-            return response.send(`${err}`)
-        });
-
-    } else if (vendorUid && productExists) {
-        return response.send(`you already have a ${productName} consider editing that product or delting it `);
-
-    }
-    else if (!vendorUid && !productExists) {
-        return response.send('get a life you stupid fuck');
+    const route = await firestore.doc(`Catagories/${Catagory}/subCatagory/${subCatagory}/subSubCatagory/${subSubCatagory}`);
+    if (!(await route.get()).exists) {
+        return response.send("The category selection doesn't exist")
     }
     else {
+        if (vendorExists && !productExists) {
+            return await firestore.collection("products").doc(`${vendorUid + productName}`).set(
+                {
+                    productName: productName,
+                    route: { Catagory: Catagory, subCatagory: subCatagory, subSubCatagory: subSubCatagory },
+                    productDescription: productDescription,
+                    hypeDescription: hypeDescription,
+                    StorageId: [],
+                    review: [],
+                    TotalAmount: 0
+                },
+            ).then(() => {
+                let ref = firestore.collection("products").doc(`${vendorUid + productName}`);
+                const writePromises: Promise<any>[] = [];
+                typesArray.forEach((element: Map<any, any>) => {
+                    writePromises.push(ref.collection("subProducts").add({ subProduct: element }));
+                },
+                );
+                writePromises.push(route
+                    .update({ Proudcts: admin.firestore.FieldValue.arrayUnion(`${vendorUid + productName}`) })
+                    .catch((err) => response.send(`${err}`)))
+                return Promise.all(writePromises);
+            }
 
-        return response.send('get a life you stupid fuck');
+            ).catch((err) => response.send(`${err}`)).then(async () => {
+
+
+
+                const reference = await firestore.doc(`vendors/${vendorUid}`).update(
+                    {
+                        productName: admin.firestore.FieldValue.arrayUnion(`${vendorUid + productName}`)
+                    },
+                );
+                return Promise.resolve(reference)
+            }).then(() => response.send("->>>")).catch((err) => {
+                console.log(err);
+                return response.send(`${err}`)
+            });
+
+        } else if (vendorUid && productExists) {
+            return response.send(`you already have a ${productName} consider editing that product or delting it `);
+
+        }
+        else if (!vendorUid && !productExists) {
+            return response.send('get a life you stupid fuck');
+        }
+        else {
+
+            return response.send('get a life you stupid fuck');
+
+        }
+    }
+});
+//calculats the amount of total available products when there is an edit a subProduct(create,update,delete)
+export const productAmountCalculator = functions.firestore.document('products/{productCollection}/subProducts/{subProductsCollection}').onWrite((change, context) => {
+
+
+
+    let ref = firestore.doc(`products/${context.params.productCollection}`)
+    return firestore.collection(`products/${context.params.productCollection}/subProducts`).get()
+        .then((array) => {
+            let totalAmount = 0;
+            array.forEach((element) => {
+                totalAmount += element.data().subProduct['amount'];
+            });
+            return ref.update({
+                TotalAmount: totalAmount
+            })
+        })
+});
+// shoudl reflect all changes performed onto products to all references
+export const onProductChange = functions.firestore.document('products/{productCollection}').onWrite(async (change, context) => {
+    const writePromises: Promise<any>[] = [];
+    
+        if (change.after.exists && change.before.exists) {
+            if (change.after.data().route != change.before.data().route) {
+                const oldRoute = change.before.data().route;
+                const newRoute = change.after.data().route;
+                const newRoute1 = await firestore.doc(`Catagories/${newRoute['Catagory']}/subCatagory/${newRoute['subCatagory']}/subSubCatagory/${newRoute['subSubCatagory']}`)
+        
+            if ((await newRoute1.get()).exists) {
+                writePromises.push(firestore.doc(`Catagories/${newRoute['Catagory']}/subCatagory/${newRoute['subCatagory']}/subSubCatagory/${newRoute['subSubCatagory']}`)
+                    .update({ Proudcts: admin.firestore.FieldValue.arrayUnion(`${context.params.productCollection}`) }))
+                writePromises.push(firestore.doc(`Catagories/${oldRoute['Catagory']}/subCatagory/${oldRoute['subCatagory']}/subSubCatagory/${oldRoute['subSubCatagory']}`)
+                    .update({ Proudcts: admin.firestore.FieldValue.arrayRemove(`${context.params.productCollection}`) }))
+            }
+            else{
+                console.log(`new route doesn't exist`)
+                writePromises.push(null);
+            }
+        }
+        else {
+            writePromises.push(null)
+        }
+        }
+        
+        else if (change.after.exists && !change.before.exists) {
+
+            writePromises.push(null)
+        }
+        else if (!change.after.exists && change.before.exists) {
+            const oldRoute = change.before.data().route;
+            writePromises.push(firestore.doc(`Catagories/${oldRoute['Catagory']}/subCatagory/${oldRoute['subCatagory']}/subSubCatagory/${oldRoute['subSubCatagory']}`)
+                .update({ Proudcts: admin.firestore.FieldValue.arrayRemove(`${context.params.productCollection}`) }))
+        }
+    
+    return Promise.all(writePromises)
+})
+
+
+//not working
+// deleteCollection(path) {
+//     firebase.firestore().collection(path).listDocuments().then(val => {
+//         val.map((val) => {
+//             val.delete()
+//         })
+//     })
+// }
+export const onProductDelete = functions.firestore.document('vendors/{vendor}').onWrite((change,context)=>{
+    const writePromises: Promise<any>[] = [];
+    if(change.after.exists && change.before.exists){
+        const oldProducts:Array<any> = change.before.data().productName;
+        const newProducts:Array<any> = change.after.data().productName;
+        const deletedProducts = new Array<any>();
+        
+        if(oldProducts.length > newProducts.length){
+            oldProducts.forEach(element => {
+                console.log(`TOBE deleted ${element}`);
+                if(!newProducts.includes(element)){
+                    console.log(`TOBE deleted ${element}`);
+                    deletedProducts.push(element);
+                }
+            });
+            deletedProducts.forEach(element=>{
+                writePromises.push(firestore.doc(`products/${element}`).delete()
+                .catch((err)=>console.log(`${err}`)));
+            })
+
+        }
+    }
+    else if(!change.after.exists && change.before.exists){
+        const oldProducts:Array<any> = change.before.data().products;
+        admin.firestore().batch().delete
+        oldProducts.forEach(element=>{
+            firestore.collection(`products`)
+            writePromises.push(firestore.collection(`products`).doc(`${element}`).delete());
+        })
 
     }
+    else if (change.after.exists && !change.before.exists) {
+
+        writePromises.push(null)
+    }
+    else{
+
+        writePromises.push(null)
+    }
+    return Promise.all(writePromises)
 
 
 
@@ -136,12 +246,16 @@ export const createProduct = functions.https.onRequest(async (request, response)
 
 
 
-});
+
+})
 
 
 export const callCatagories = functions.https.onRequest(async (request, response) => {
 
     const fire = request.query.fire;
+    if (fire) {
+        response.send("hell blade")
+    }
 
     var categories = {
         "Phone and Accessories": {
@@ -195,18 +309,32 @@ export const callCatagories = functions.https.onRequest(async (request, response
         }
     };
 
-    for (let x in categories){
-        for(let y in categories[x]){
-            console.log(y)
-            for(let z in categories[x][y]){
-                console.log(z)
+    const writePromises: Promise<any>[] = [];
+    for (const catagory in categories) {
+        writePromises.push(firestore.doc(`Catagories/${catagory}`).set({}).then(() => { console.log(`${catagory}`); return response.send(`${catagory}`) }).catch((err) => { return response.send(`${err}`) }))
+        for (let subCategory in categories[catagory]) {
+            writePromises.push(firestore.doc(`Catagories/${catagory}/subCatagory/${subCategory}`).set({}).then(() => { console.log(`${subCategory}`); return response.send(`${subCategory}`) }).catch((err) => { return response.send(`${err}`) }));
+            for (let subSubCategory in categories[catagory][subCategory]) {
+                writePromises.push(firestore.doc(`Catagories/${catagory}/subCatagory/${subCategory}/subSubCatagory/${subSubCategory}`)
+                    .set({})
+                    .then(() => {
+                        console.log(`${subSubCategory}`);
+                        return response.send(`${subCategory}`)
+                    })
+                    .catch((err) => {
+                        return response.send(`${err}`)
+                    }));
+
+
+
+
             }
+
         }
     }
 
-    
 
-
+    return Promise.all(writePromises);
 
 
 
@@ -224,6 +352,7 @@ export const callCatagories = functions.https.onRequest(async (request, response
 
 
 export const test = functions.https.onRequest(async (request, response) => {
+
 
 
 
